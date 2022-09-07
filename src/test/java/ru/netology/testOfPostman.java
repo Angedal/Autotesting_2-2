7e7e7bdfd13551a546a0ea28d61f019055d88855
Julia Kostomarova <kostomarova.julia@gmail.com>
2022-09-07 16:32:17 +0400

    Fixed test again

--- src/test/java/ru/netology/testOfPostman.java
+++ src/test/java/ru/netology/testOfPostman.java
@@ -12,13 +12,13 @@ class testOfPostman {
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some info") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
         // Выполняемые действия
                 .when()
                 .post("/post")
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some info"));
+                .body("data", equalTo("some data"));
         }
     }
